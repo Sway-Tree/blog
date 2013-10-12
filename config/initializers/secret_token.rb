5920f8699130dc4196b0798509b9042c7cf4d11e
Sway-Tree <agnieszka.dudek92@gmail.com>
2013-10-12 19:54:09 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'cdc59dcdb502f81f2b1b9218a3df8fac440243238d3fe621484b38a4eacb221bc08339d461abf85946f9cb5bf6d998b1ea374a2c3ac1e1381b3427cda4db7e40'
